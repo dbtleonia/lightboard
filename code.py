6be93343dd096a0e47da49a2f85353be376b56a1
David B. Tucker <dbtleonia@gmail.com>
2023-02-25 10:15:08 -0500

    Today's weather, more greetings, sleep at night.

--- code.py
+++ code.py
@@ -27,16 +27,17 @@ DATA_SOURCE = '{}?lat={}&lon={}&appid={}&exclude=minutely,daily,alerts&units=imp
 # --- Display setup ---
 matrix = Matrix()
 display = matrix.display
-network = Network(status_neopixel=board.NEOPIXEL, debug=False)
+network = Network()
 
 font_thumb = bitmap_font.load_font('fonts/tom-thumb.bdf')
 
+mtgroup = displayio.Group()
 wxgroup = displayio.Group()
 lbl_greet = Label(font=terminalio.FONT, x=0, y=3, color=0xFF6600)
 lbl_temp = Label(font=terminalio.FONT, x=18, y=15, color=0xBB56FF)
 lbl_feel = Label(font=font_thumb, x=8, y=18, color=0xBB56FF)
-lbl_hi = Label(font=font_thumb, x=32, y=13, color=0xFFCC00)
-lbl_lo = Label(font=font_thumb, x=32, y=20, color=0xFFCC00)
+lbl_hi = Label(font=font_thumb, x=32, y=13)
+lbl_lo = Label(font=font_thumb, x=32, y=20)
 lbl_time = Label(font=terminalio.FONT, x=0, y=27, color=0xBB56FF)
 wxgroup.append(lbl_greet)
 wxgroup.append(lbl_temp)
@@ -47,9 +48,14 @@ wxgroup.append(lbl_time)
 wxgroup.append(displayio.Group()) # for temp graph
 wxgroup.append(displayio.Group()) # for rain graph
 
-def update_weather(value):
+def update_weather(value, show_today):
     now = time.localtime(time.time() + timezone_offset)
-    for_day = (now.tm_wday + 1) % 7
+    if show_today:
+        for_day = now.tm_wday
+        rcolor = 0x5D2B7F
+    else:
+        for_day = (now.tm_wday + 1) % 7  # tomorrow
+        rcolor = 0xBB56FF
     temps = []
     poppx = []
     for hour in value['hourly']:
@@ -67,9 +73,12 @@ def update_weather(value):
     lbl_feel.text = '{:2d}'.format(round(value['current']['feels_like']))
     lbl_hi.text = '{:2d}'.format(round(hi))
     lbl_lo.text = '{:2d}'.format(round(lo))
+    lbl_hi.color = rcolor
+    lbl_lo.color = rcolor
 
-    tempgroup = displayio.Group(x=40, y=11)
-    raingroup = displayio.Group(x=40, y=22)
+    xoffset = 64 - len(temps)
+    tempgroup = displayio.Group(x=xoffset, y=11)
+    raingroup = displayio.Group(x=xoffset, y=22)
     wxgroup[-2] = tempgroup
     wxgroup[-1] = raingroup
     gc.collect()
@@ -81,14 +90,17 @@ def update_weather(value):
             b = min(n, temppx[i-1]+1)
         else:
             b = min(n, temppx[i-1]+1, temppx[i+1]+1)
-        tempgroup.append(Line(i, 10-n, i, 10-b, 0xFFCC00))
+        tempgroup.append(Line(i, 10-n, i, 10-b, rcolor))
 
     for i, pop in enumerate(poppx):
         raingroup.append(Line(i, 10-pop, i, 10, 0x1E3559))
 
 localtime_refresh = None
 weather_refresh = None
+weather_hour = None
 timezone_offset = None
+now = None
+
 while True:
     # Only query the online time once per hour (and on first run)
     if (not localtime_refresh) or (time.monotonic() - localtime_refresh) > 3600:
@@ -100,26 +112,57 @@ while True:
             print('Error fetching time:', e)
             continue
 
-    # Only query the weather every 10 minutes (and on first run)
-    if (not weather_refresh) or (time.monotonic() - weather_refresh) > 600:
+    # Query the weather, when:
+    # - it's the first run, or
+    # - the data is older than 10 minutes, or
+    # - it's the top of the hour
+    if ((not weather_refresh) or
+        (time.monotonic() - weather_refresh) > 600 or
+        (now and weather_hour != now.tm_hour)):
         try:
             value = network.fetch_data(DATA_SOURCE, json_path=([],))
             timezone_offset = value['timezone_offset']
-            update_weather(value)
+            now = time.localtime(time.time() + timezone_offset)
+            update_weather(value, show_today=now.tm_hour < 17)
             weather_refresh = time.monotonic()
+            weather_hour = now.tm_hour
         except RuntimeError as e:
             print('Error fetching weather:', e)
             continue
 
-    now = time.localtime(time.time() + timezone_offset)
-    if now.tm_hour >= 17 and now.tm_hour < 19:
-        lbl_greet.text = 'Happy hour!'
-    elif now.tm_hour >= 19 and now.tm_hour < 22:
-        lbl_greet.text = 'Wind down'
-    elif now.tm_hour == 22 and now.tm_min < 30:
-        lbl_greet.text = 'Tuck in!'
+    now_unix = time.time()
+    now = time.localtime(now_unix + timezone_offset)
+
+    # TODO: Sleep smarter.
+    if ((now.tm_hour == 22 and now.tm_min >= 30) or
+        (now.tm_hour == 23) or
+        (now.tm_hour < 7)):
+        display.show(mtgroup)
+        time.sleep(60)
     else:
-        lbl_greet.text = 'Helo world'
-    lbl_time.text = "{:2d}:{:02d}".format(now.tm_hour%12, now.tm_min)
-    display.show(wxgroup)
-    time.sleep(1)
+      if now.tm_hour < 12:
+          lbl_greet.text = [
+              'Kali mera!',  # Monday
+              'Bonjour!',
+              "G'morning!",
+              'Bom dia!',
+              'Co-brkfst!',
+              'Swan time!',
+              'Buongiorno!',
+          ][now.tm_wday]
+      elif now.tm_hour < 17:
+          days = ((secrets['event_time'] - now_unix) // (60 * 60 * 24)) + 1
+          lbl_greet.text = '{:3d} to {}!'.format(days, secrets['event_name'])
+       elif now.tm_hour < 19:
+          lbl_greet.text = 'Happy hour!'
+      elif now.tm_hour < 20:
+          lbl_greet.text = 'Dinnertime!'
+      elif now.tm_hour < 22:
+          lbl_greet.text = 'Wind down!'
+      elif now.tm_hour == 22 and now.tm_min < 30:
+          lbl_greet.text = 'Tuck in!'
+      else:
+          lbl_greet.text = 'Helo world!'
+      lbl_time.text = "{:2d}:{:02d}".format(now.tm_hour%12, now.tm_min)
+      display.show(wxgroup)
+      time.sleep(1)
